936659178cd4a2bd7c0b62fc93c68a1e81afdba6
rakuhasan2017 <rakuhasan2017@gmail.com>
2026-04-21 21:04:44 +0600

    : update Prometheus middleware import path

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authroute.js';
 import cors from 'cors';
-import { setupPrometheus } from './config/prometheus.js';
+import { setupPrometheus } from './middleware/prometheusmiddleware.js';
 
 dotenv.config();
 
